d5a4914e9cf73c149a7bbfa46c14cb16debb66e4
agtfsfhgsdf <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-03-08 04:02:01 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,11 +1,11 @@
 const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
-const student = new Student();	
-var id=0;
+const student = new Student();
+const myClass = new MyClass();
 test("Test MyClass's addStudent", ()=>{
     // TODO      
-        this.students = [];
+        this.students = ['John', 'Jane', 'Doe', 'Smith'];
         if (!(student instanceof Student)) {
             return -1;
         }
@@ -15,20 +15,26 @@ test("Test MyClass's addStudent", ()=>{
 }); 
 test("Test MyClass's getStudentById", () => {
     // TODO 
-    return this.students[id];
+    const newStudentId = myClass.addStudent(student);
+    const newStudentName = myClass.getStudentById(newStudentId).getName();
+    return this.students[0];
+    return this.students[1];
+    return this.students[2];
+    return this.students[3];
     throw new Error("Test not implemented");
 });
 test("Test Student's setName", () => {
     // TODO
+        student.setName('John');
         if (typeof userName !== 'string') {
             return;
         }   
-        this.name = userName;
-      
+        this.name = userName;      
     throw new Error("Test not implemented");
 });
 test("Test Student's getName", () => {
     // TODO
+        student.getName();
         if (this.name === undefined) {
             return '';
         }
