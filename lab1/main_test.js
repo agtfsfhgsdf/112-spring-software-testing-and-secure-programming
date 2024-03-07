98912e62d21fb3b4eeb8be64283207500d9cebdb
agtfsfhgsdf <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-03-08 05:16:39 +0800

    Update main_test.js

--- lab1/main_test.js
+++ lab1/main_test.js
@@ -1,40 +1,46 @@
 const test = require('node:test');
 const assert = require('assert');
 const { MyClass, Student } = require('./main');
-const student = new Student();
-const myClass = new MyClass();
+
 test("Test MyClass's addStudent", ()=>{
-    // TODO      
+    // TODO  
+    	const student = new Student();
+	const myClass = new MyClass();    
         student.setName("John");
         const newStudentId = myClass.addStudent(student);
         assert.strictEqual(newStudentId, 0,"正確");
         const notstudentID=111;
         const newStudentId2 = myClass.addStudent(notstudentID);
         assert.strictEqual(newStudentId2,-1,"錯誤");
-       throw new Error("Test not implemented");
+       
 }); 
 test("Test MyClass's getStudentById", () => {
     // TODO 
+    const student = new Student();
+    const myClass = new MyClass();  
     student.setName("kevin");
     myClass.addStudent(student);
     const newStudentName = myClass.getStudentById(0);
     assert.strictEqual(newStudentName.getName(), "kevin","正確");
     newStudentName = myClass.getStudentById(-1);
     assert.strictEqual(newStudentName, null,"錯誤");
-    throw new Error("Test not implemented");
+   
 });
 test("Test Student's setName", () => {
     // TODO
+        const student = new Student();
+	const myClass = new MyClass();  
         student.setName('John');
         assert.strictEqual(student.name, "John","正確");
         student.setName(111);
         assert.strictEqual(student.name,111,"錯誤");    
-    throw new Error("Test not implemented");
+    
 });
 test("Test Student's getName", () => {
     // TODO
+       const student = new Student();
+	const myClass = new MyClass();  
     student.setName('John');    
     assert.strictEqual(student.getName(), "John","正確");
     assert.strictEqual(student.getName(),'',"錯誤");
-        throw new Error("Test not implemented");    
 }); 
