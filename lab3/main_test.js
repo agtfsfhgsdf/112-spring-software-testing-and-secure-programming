8e17376d373bd754da9d92a7604499457e5b6e06
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-04-19 11:12:36 +0800

    Update main_test.js

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -3,52 +3,45 @@ const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
-
-describe("Test", () => {
+it("Test Calculator log", () => {   
     const calculator = new Calculator();
-    it("exp() fun Test", () => {
-        let expTest = [
-            { param: 87, expected: Math.exp(87)},
-            { param: 0, expected: Math.exp(0) },
-            { param: -1, expected: Math.exp(-1) },
-            { param: '48763', expected: Error, msg: "unsupported operand type" },
-            { param: true, expected: Error, msg: "unsupported operand type" },
-			{ param: null, expected: Error, msg: 'unsupported operand type'},
-            { param: Infinity, expected: Error, msg: "unsupported operand type" },
-			{ param: -Infinity, expected: Error, msg: 'unsupported operand type'},
-            { param: Number.MAX_VALUE, expected: Error, msg: "over" }
-			
-        ];
+    const testcase = [
+        {num: 5, log_expected: Math.log(5)},
+        {num: 37, log_expected: Math.log(37)},
+        {num: 52, log_expected: Math.log(52)}
+    ];
+    for (const test of testcase) {
+        assert.strictEqual(calculator.log(test.num), test.log_expected);
+    }
 
-        expTest.map(({ param, expected, msg }) => {
-            if (expected === Error) {
-                assert.throws(() => calculator.exp(param), expected, msg);
-            }
-            else {
-                assert.strictEqual(calculator.exp(param), expected);
-            }
-        });
-    });
+    const test_error = [
+        {num: 'test', log_expected: 'unsupported operand type'},
+        {num: 0, log_expected: 'math domain error (1)'},
+        {num: -1, log_expected: 'math domain error (2)'}
+    ]
 
-    it("log() fun Test", () => {
-        let logTestcase = [
-            { param: 87, expected: Math.log(87) },
-            { param: 48763, expected: Math.log(48763) },
-            { param: '48763', expected: Error, msg: "unsupported operand type" },
-            { param: true, expected: Error, msg: "unsupported operand type" },
-			{ param: null, expected: Error, msg: 'unsupported operand type'},
-            { param: Infinity, expected: Error, msg: "unsupported operand type" },
-            { param: 0, expected: Error, msg: "math domain error" },
-            { param: -1, expected: Error, msg: "math domain error" },
-        ];
+    for (const test of test_error) {
+        assert.throws(() => calculator.log(test.num), new Error(test.log_expected));
+    }
+});
 
-        logTestcase.map(({ param, expected, msg }) => {
-            if (expected === Error) { 
-                assert.throws(() => calculator.log(param), expected, msg);
-            }
-            else {
-                assert.strictEqual(calculator.log(param), expected);
-            }
-        });
-    });
+it("Test Calculator exp", () => {   
+    const calculator = new Calculator();
+    const testcase = [
+        {num: 5, exp_expected: Math.exp(5)},
+        {num: 37, exp_expected: Math.exp(37)},
+        {num: 52, exp_expected: Math.exp(52)}
+    ];
+    for (const test of testcase) {
+        assert.strictEqual(calculator.exp(test.num), test.exp_expected);
+    }
+
+    const test_error = [
+        {num: 'test', exp_expected: 'unsupported operand type'},
+        {num: 10000, exp_expected: 'overflow'}
+    ]
+
+    for (const test of test_error) {
+        assert.throws(() => calculator.exp(test.num), new Error(test.exp_expected));
+    }
 });
