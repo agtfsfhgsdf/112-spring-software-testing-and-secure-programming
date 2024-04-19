f13b3e1237b3f2908a9241cda9ae2620bb7f9abb
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-04-19 10:59:16 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -8,14 +8,17 @@ const puppeteer = require('puppeteer');
     // Navigate the page to a URL
     await page.goto('https://pptr.dev/');
 
-    // Hints:
-    // Click search button
-    // Type into search box
-    // Wait for search result
-    // Get the `Docs` result section
-    // Click on first result in `Docs` section
-    // Locate the title
-    // Print the title
+    await page.setViewport({width:1080 , height:1024 });
+    const searchResultSelector = '.DocSearch-Button';
+    await page.waitForSelector(searchResultSelector);
+    await page.click(searchResultSelector);
+    await page.waitForSelector('.DocSearch-Input'); 
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 100});
+    await page.waitForSelector('#docsearch-item-5');
+    await page.click('#docsearch-item-5');
+    const titleSelector = await page.waitForSelector('h1');
+    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
+    console.log(fullTitle);
 
     // Close the browser
     await browser.close();
