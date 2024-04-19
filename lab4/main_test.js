418aaf090c1419ac7ead0b5387df7396cb68eb6e
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-04-19 11:24:01 +0800

    Update main_test.js

--- lab4/main_test.js
+++ lab4/main_test.js
@@ -1,34 +1,42 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // Launch the browser and open a new blank page
+
+  
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    // Navigate the page to a URL
-    await page.goto('https://pptr.dev/');
-    
-    await page.setViewport({width:1080 , height:1024 });
-    
-    
-    const searchResultSelector = '.DocSearch-Button';
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    await page.goto('https://pptr.dev/webdriver-bidi/#measuring-progress');
 
+    
+    await page.waitForSelector('.DocSearch-Button-Placeholder'); 
+    await page.click('.DocSearch-Button-Placeholder'); 
+  
     
     await page.waitForSelector('.DocSearch-Input'); 
-    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa', {delay: 100});
+    await page.type('.DocSearch-Input', 'chipi chipi chapa chapa'); //input
+    await page.waitForSelector('.DocSearch-Hits')
+    
+   
+    await page.waitForFunction(() => {
+        
+        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
+        return hit && hit.closest('.DocSearch-Hit-Container');
+    });
 
     
-    await page.waitForSelector('#docsearch-item-5');
-    await page.click('#docsearch-item-5');
+    await page.evaluate(() => {
+        const hit = Array.from(document.querySelectorAll('.DocSearch-Hit-title')).find(e => e.textContent.includes('To gauge the capabilities of'));
+        hit && hit.closest('.DocSearch-Hit-Container').click();
+    });
 
+
+    await page.waitForSelector('h1', { visible: true });
     
-    const titleSelector = await page.waitForSelector('h1');
-    const fullTitle = await titleSelector?.evaluate(el => el.textContent);
-    // Print the title
-    console.log(fullTitle);
+    // output
+    const headerText = await page.$eval('h1', element => element.textContent);
+    console.log(headerText);  
 
-    // Close the browser
     await browser.close();
+
 })();
