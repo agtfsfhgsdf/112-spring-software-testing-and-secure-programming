58ce31dcf7b84433a94d40e201d73505e66aa224
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-05-03 15:33:42 +0800

    Update sol.py

--- lab7/sol.py
+++ lab7/sol.py
@@ -1,18 +1,14 @@
 import angr, sys
-
 def success_condition(state):
-    return b"Login successful" in state.posix.dumps(sys.stdout.fileno())
-
+    return b"successful" in state.posix.dumps(sys.stdout.fileno())
 def fail_condition(state):
-    return b"Login failed" in state.posix.dumps(sys.stdout.fileno())
-
-proj = angr.Project("./login")
-init_state = proj.factory.entry_state()
-simulation = proj.factory.simgr(init_state)
+    return b"failed" in state.posix.dumps(sys.stdout.fileno())
+pr = angr.Project("./login")
+init_state = pr.factory.entry_state()
+simulation = pr.factory.simgr(init_state)
 simulation.explore(find=success_condition, avoid=fail_condition)
-
 if simulation.found:
     for found_state in simulation.found:
         print(found_state.posix.dumps(sys.stdin.fileno()))
 else:
-    print("No valid input found.")
+    print("No valid")
