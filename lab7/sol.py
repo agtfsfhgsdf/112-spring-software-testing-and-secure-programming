a7610b33d3a3f1e05e1d758921b69db94dcfef65
TAO KUO HUA <131828636+agtfsfhgsdf@users.noreply.github.com>
2024-05-15 22:17:16 +0800

    Update sol.py

--- lab7/sol.py
+++ lab7/sol.py
@@ -1,14 +1,11 @@
 import angr, sys
 def success_condition(state):
-    return b"successful" in state.posix.dumps(sys.stdout.fileno())
+    return  b"Login successful" in state.posix.dumps(sys.stdout.fileno())
 def fail_condition(state):
-    return b"failed" in state.posix.dumps(sys.stdout.fileno())
-pr = angr.Project("./login")
-init_state = pr.factory.entry_state()
-simulation = pr.factory.simgr(init_state)
-simulation.explore(find=success_condition, avoid=fail_condition)
-if simulation.found:
-    for found_state in simulation.found:
-        print(found_state.posix.dumps(sys.stdin.fileno()))
-else:
-    print("No valid")
+    return b"Login failed" in state.posix.dumps(sys.stdout.fileno())
+pj = angr.Project('./login')
+init_state = pj.factory.entry_state()
+sim = pj.factory.simgr(init_state)
+sim.explore(find = success_condition, avoid = fail_condition)
+solution = sim.found[0]
+print(solution.posix.dumps(sys.stdin.fileno()))
